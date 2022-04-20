bde4406a859105d34adc5ba36fde6d8dbddf61e8
Marko Hine <hinemp@dukes.jmu.edu>
2022-04-20 15:46:09 -0400

    initial commit

---
+++ API Tools/weatherman/bot.js
@@ -0,0 +1,11 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
